957020f2f99e19b39a61c9db924320602962eb92
Tim Gordon <timothygordon32@gmail.com>
2014-09-20 08:27:38 +0100

    Clean up Task JSON serialization spec.

--- test/TaskSpec.scala
+++ test/TaskSpec.scala
@@ -1,21 +1,21 @@
+import models.Task
+import models.Task._
 import org.specs2.mutable._
 import play.api.libs.json._
 import reactivemongo.bson.BSONObjectID
 
-import models.Task
-import models.Task._
-
 class TaskSpec extends Specification {
 
-  val task = Task(Some(BSONObjectID.generate), "I'm new")
-
   "JSON serialization" should {
-    "round trip validation and parse of JSON to recreate serialized object" in {
-      Json.toJson(task).validate[Task].get must be equalTo task
+    "should serialize to JSON" in {
+      val id = Some(BSONObjectID.generate)
+      val expectedJson = Json.obj("id" -> id.get.stringify, "label" -> "I'm new")
+      Json.toJson(Task(id, "I'm new")) must be equalTo expectedJson
     }
 
-    "should serialize to JSON" in {
-      Json.obj("id" -> task.id.get.stringify, "label" -> task.label) must be_==(Json.toJson(task))
+    "round trip validation and parse of JSON to recreate serialized object" in {
+      val task = Task(Some(BSONObjectID.generate), "I'm new")
+      Json.toJson(task).validate[Task].get must be equalTo task
     }
   }
 }
