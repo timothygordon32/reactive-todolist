35be8718f567de52bfdea4f0cd5f1b72a20aa8c2
Tim Gordon <timothygordon32@gmail.com>
2014-10-16 21:48:45 +0100

    Workaround deprecation warnings with Secure Social.

--- app/controllers/CustomMailTemplates.scala
+++ app/controllers/CustomMailTemplates.scala
@@ -2,25 +2,24 @@ package controllers
 
 import play.api.i18n.Lang
 import play.api.mvc.RequestHeader
-import play.api.templates.{Html, Txt}
 import securesocial.controllers.MailTemplates
 import securesocial.core.{IdentityProvider, BasicProfile}
 import views.html.email.signUpHtml
 import views.txt.email.signUpText
 
 object CustomMailTemplates extends MailTemplates {
-  override def getSignUpEmail(token: String)(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = {
+  override def getSignUpEmail(token: String)(implicit request: RequestHeader, lang: Lang) = {
     val link = s"${routes.Home.index().absoluteURL(IdentityProvider.sslEnabled)}#/signup/$token"
     (Some(signUpText(link)), Some(signUpHtml(link)))
   }
 
-  override def getUnknownEmailNotice()(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = ???
+  override def getUnknownEmailNotice()(implicit request: RequestHeader, lang: Lang) = ???
 
-  override def getSendPasswordResetEmail(user: BasicProfile, token: String)(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = ???
+  override def getSendPasswordResetEmail(user: BasicProfile, token: String)(implicit request: RequestHeader, lang: Lang) = ???
 
-  override def getPasswordChangedNoticeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = ???
+  override def getPasswordChangedNoticeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = ???
 
-  override def getWelcomeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = ???
+  override def getWelcomeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = ???
 
-  override def getAlreadyRegisteredEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang): (Option[Txt], Option[Html]) = ???
+  override def getAlreadyRegisteredEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = ???
 }
