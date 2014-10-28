d01220bd58c7702306ba930b3b09d8e278f19fcb
Tim Gordon <timothygordon32@gmail.com>
2014-10-28 20:51:58 +0000

    Upgrade reactivemongo to 0.10.5.0.akka23

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -8,16 +8,15 @@ object ApplicationBuild extends Build with Application {
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.reactivemongo" %% "reactivemongo" % "0.10.5.akka23-SNAPSHOT",
-    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.akka23-SNAPSHOT",
+    "org.reactivemongo" %% "reactivemongo" % "0.10.5.0.akka23",
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.0.akka23",
     "org.mindrot" % "jbcrypt" % "0.3m",
     "ws.securesocial" %% "securesocial" % "3.0-M1",
     "com.typesafe.play" %% "play-ws" % "2.3.3" % "test,it",
     "com.typesafe.play" %% "play-test" % "2.3.3" % "test,it")
 
   val appResolvers = Seq(
-    "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
-    "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/")
+    "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/")
 
   lazy val plugins = Seq(
     play.PlayScala)
