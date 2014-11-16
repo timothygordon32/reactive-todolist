40914fb0f48b3139a68702da2c8252cae66b19d2
Tim Gordon <timothygordon32@gmail.com>
2014-11-16 14:51:22 +0000

    Logoff consitent with SecureSocial.

--- app/controllers/Authenticator.scala
+++ app/controllers/Authenticator.scala
@@ -3,7 +3,9 @@ package controllers
 import models.User
 import play.api.libs.json.Json
 import play.api.mvc._
-import securesocial.core.{RuntimeEnvironment, SecureSocial}
+import securesocial.core.{LogoutEvent, Events, RuntimeEnvironment, SecureSocial}
+
+import scala.concurrent.Future
 
 class Authenticator(override implicit val env: RuntimeEnvironment[User]) extends Controller with SecureSocial[User] {
 
@@ -11,7 +13,13 @@ class Authenticator(override implicit val env: RuntimeEnvironment[User]) extends
     Ok(Json.toJson[User](request.user))
   }
 
-  def logoff = SecuredAction { request =>
-    NoContent.discardingCookies(DiscardingCookie("id"))
+  def logoff = UserAwareAction.async { implicit request =>
+    val result = for {
+      user <- request.user
+      authenticator <- request.authenticator
+    } yield {
+      authenticator.discarding(NoContent.withSession(Events.fire(new LogoutEvent(user)).getOrElse(request.session)))
+    }
+    result.getOrElse(Future.successful(Unauthorized))
   }
 }
