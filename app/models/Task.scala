60f5657c7b7d091992873d92106df7d0555d8dc9
Tim Gordon <timothygordon32@gmail.com>
2014-11-21 18:58:20 +0000

    Task ids optional on creation.

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -3,7 +3,7 @@ package models
 import play.api.libs.json._
 import reactivemongo.bson._
 
-case class Task(id: Option[BSONObjectID], text: String, done: Boolean = false)
+case class Task(id: Option[BSONObjectID] = None, text: String, done: Boolean = false)
 
 object Task {
   implicit val taskFormat = {
