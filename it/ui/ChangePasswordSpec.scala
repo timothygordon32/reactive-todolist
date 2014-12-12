69acd1adafeb0d06ae32fe7a954b8806bc2854eb
Tim Gordon <timothygordon32@gmail.com>
2014-12-12 18:51:39 +0000

    Extract trait for ITs with browsers and email.

--- it/ui/ChangePasswordSpec.scala
+++ it/ui/ChangePasswordSpec.scala
@@ -1,28 +1,45 @@
 package ui
 
+import org.openqa.selenium.WebDriver
 import org.subethamail.wiser.Wiser
-import play.api.test.{FakeApplication, PlaySpecification, WebDriverFactory, WithBrowser}
+import play.api.test._
 import ui.model.User.User3
 import ui.model.{LoginPage, SecurityMessageMatchers, SecurityMessages}
 import utils.UniqueStrings
+
+abstract class WithMailServerAndBrowser(
+    override val webDriver: WebDriver = WebDriverFactory(Helpers.HTMLUNIT),
+    override val port: Int = Helpers.testServerPort,
+    val mailServer: Wiser with SecurityMessages)
+  extends WithBrowser(
+    webDriver = webDriver, app = FakeApplication(
+    additionalConfiguration = Map(
+      "smtp.host" -> mailServer.getServer.getHostName,
+      "smtp.port" -> mailServer.getServer.getPort)),
+    port = port)
+
+class FakeMakeServer(hostname: String, port: Int) extends Wiser with SecurityMessages {
+  setHostname(hostname)
+  setPort(port)
+
+  def started = {
+    start()
+    this
+  }
+}
+
+object FakeMakeServer {
+  def apply(hostname: String, port: Int) = new FakeMakeServer(hostname, port)
+}
 
 class ChangePasswordSpec extends PlaySpecification with UniqueStrings with SecurityMessageMatchers {
 
   "Task page" should {
 
-    "allow changing of password" in new WithBrowser(
+    "allow changing of password" in new WithMailServerAndBrowser(
       webDriver = WebDriverFactory(FIREFOX),
-      app = FakeApplication(
-        additionalConfiguration = Map(
-          "smtp.host" -> "localhost",
-          "smtp.port" -> 10027)
-      ),
+      mailServer = FakeMakeServer("localhost", 10027).started,
       port = 19004) with LoginPageSugar {
-
-      val mailServer = new Wiser with SecurityMessages
-      mailServer.setHostname("localhost")
-      mailServer.setPort(10027)
-      mailServer.start()
 
       val taskPage = browser goTo loginPage login User3
       val changePasswordPage = taskPage changePassword()
