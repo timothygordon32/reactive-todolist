6531373f4d9bd3769e1d264c5f82b72ae51cbb03
Tim Gordon <timothygordon32@gmail.com>
2014-12-20 11:55:35 +0000

    Find an HTTP header authenticator.

--- it/repository/AuthenticatorStoreSpec.scala
+++ it/repository/AuthenticatorStoreSpec.scala
@@ -1,25 +1,29 @@
 package repository
 
 import models.User
+import play.api.libs.json.Json
 import play.api.test.PlaySpecification
 import reactivemongo.api.indexes.IndexType
-import securesocial.core.authenticator.{CookieAuthenticator, IdGenerator}
+import securesocial.core.authenticator.{CookieAuthenticator, HttpHeaderAuthenticator, IdGenerator}
 import securesocial.core.services.SaveMode
 import time.DateTimeUtils._
 import utils.StartedFakeApplication
+
+import scala.concurrent.Future
+import scala.reflect.ClassTag
 
 class AuthenticatorStoreSpec extends PlaySpecification with StartedFakeApplication {
 
   "Authenticator store" should {
 
     "find a cookie authenticator" in new AuthenticatorTestCase {
-      val user = await(store.save(profile, SaveMode.SignUp))
+      val user = await(cookieStore.save(profile, SaveMode.SignUp))
       val created = now
       val expire = created.plusHours(1)
       val lastUsed = created.plusMillis(1)
-      await(store.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, store), 3600))
+      await(cookieStore.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, cookieStore), 3600))
 
-      val authenticator = await(store.find(authenticatorId)).get
+      val authenticator = await(cookieStore.find(authenticatorId)).get
 
       authenticator.user must be equalTo user
       authenticator.expirationDate must be equalTo expire
@@ -28,40 +32,69 @@ class AuthenticatorStoreSpec extends PlaySpecification with StartedFakeApplicati
     }
 
     "delete a cookie authenticator" in new AuthenticatorTestCase {
-      val user = await(store.save(profile, SaveMode.SignUp))
+      val user = await(cookieStore.save(profile, SaveMode.SignUp))
       val created = now
       val expire = created.plusHours(1)
       val lastUsed = created.plusMillis(1)
-      await(store.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, store), 3600))
+      await(cookieStore.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, cookieStore), 3600))
 
-      await(store.delete(authenticatorId))
+      await(cookieStore.delete(authenticatorId))
 
-      await(store.find(authenticatorId)) must be equalTo None
+      await(cookieStore.find(authenticatorId)) must be equalTo None
     }
 
     "not find an expired cookie authenticator" in new AuthenticatorTestCase {
-      val user = await(store.save(profile, SaveMode.SignUp))
+      val user = await(cookieStore.save(profile, SaveMode.SignUp))
       val created = now.minusHours(1)
       val expire = created.plusHours(1)
       val lastUsed = created
-      await(store.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, store), 3600))
+      await(cookieStore.save(CookieAuthenticator(authenticatorId, user, expire, lastUsed, created, cookieStore), 3600))
 
-      val authenticator = await(store.find(authenticatorId))
+      val authenticator = await(cookieStore.find(authenticatorId))
 
-      await(store.find(authenticatorId)) must be equalTo None
+      await(cookieStore.find(authenticatorId)) must be equalTo None
     }
 
     "have an index on authenticator id" in new AuthenticatorTestCase {
-      val indexes = await(store.indexes())
+      val indexes = await(cookieStore.indexes())
       indexes.filter(_.key == Seq("authenticator.id" -> IndexType.Ascending)) must not be empty
     }
 
-    "find a http header authenticator" in pending
+    "find a http header authenticator" in new AuthenticatorTestCase {
+      val user = await(httpHeaderStore.save(profile, SaveMode.SignUp))
+      val created = now
+      val expire = created.plusHours(1)
+      val lastUsed = created.plusMillis(1)
+      await(httpHeaderStore.save(HttpHeaderAuthenticator(authenticatorId, user, expire, lastUsed, created, httpHeaderStore), 3600))
+
+      val authenticator = await(httpHeaderStore.find(authenticatorId)).get
+
+      authenticator.user must be equalTo user
+      authenticator.expirationDate must be equalTo expire
+      authenticator.lastUsed must be equalTo lastUsed
+      authenticator.creationDate must be equalTo created
+    }
   }
 
   trait AuthenticatorTestCase extends ProfileTestCase {
     val authenticatorId = await(new IdGenerator.Default().generate)
 
-    val store = new ProfileAuthenticatorStore[CookieAuthenticator[User]] {}
+    val cookieStore = new ProfileAuthenticatorStore[CookieAuthenticator[User]] {}
+
+    val httpHeaderStore = new ProfileAuthenticatorStore[HttpHeaderAuthenticator[User]] {
+      override def find(id: String)(implicit ct: ClassTag[HttpHeaderAuthenticator[User]]): Future[Option[HttpHeaderAuthenticator[User]]] = {
+        val selector = Json.obj("authenticator.id" -> id, "authenticator.expirationDate" -> Json.obj("$gte" -> now))
+        collection.find(selector).cursor[UserWithAuthenticator].headOption.map(_.map {
+          userWithAuthenticator =>
+            HttpHeaderAuthenticator(
+              id,
+              userWithAuthenticator.toUser,
+              userWithAuthenticator.authenticator.expirationDate,
+              userWithAuthenticator.authenticator.lastUsed,
+              userWithAuthenticator.authenticator.creationDate,
+              this)
+        })
+      }
+    }
   }
 }
