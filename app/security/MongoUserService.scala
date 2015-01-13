ffe8ea0b0798c6cd38ac3ac83de5772a4cb6d9ab
Tim Gordon <timothygordon32@gmail.com>
2015-01-13 08:02:35 +0000

    Use trait to express token repository dependency.

--- app/security/MongoUserService.scala
+++ app/security/MongoUserService.scala
@@ -2,16 +2,25 @@ package security
 
 import models.User
 import reactivemongo.api.indexes.Index
-import repository.{ProfileRepository, TokenRepository}
+import repository.{MongoTokenRepositoryComponent, ProfileRepository}
 import securesocial.core.services.UserService
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class MongoUserService extends UserService[User] with ProfileRepository with TokenRepository {
-  override def indexes(): Future[Seq[Index]] = {
-    val profileIndexes = super[ProfileRepository].indexes()
-    val tokenIndexes = super[TokenRepository].indexes()
-    Future.sequence(Seq(profileIndexes, tokenIndexes)).map(_.flatten)
+trait SecureSocialUserServiceComponent {
+  val userService: UserService[User]
+}
+
+trait MongoSecureSocialUserServiceComponent extends SecureSocialUserServiceComponent {
+  self: MongoTokenRepositoryComponent =>
+
+  class MongoUserService extends UserService[User] with ProfileRepository with MongoTokenRepository {
+    override def indexes(): Future[Seq[Index]] = {
+      val profileIndexes = super[ProfileRepository].indexes()
+      val tokenIndexes = super[MongoTokenRepository].indexes()
+      Future.sequence(Seq(profileIndexes, tokenIndexes)).map(_.flatten)
+    }
   }
 }
+
--- app/security/SecurityEnvironment.scala
+++ app/security/SecurityEnvironment.scala
@@ -2,14 +2,20 @@ package security
 
 import controllers.{JsonViewTemplates, CustomMailTemplates}
 import models.User
+import repository.MongoTokenRepositoryComponent
 import securesocial.controllers.{ViewTemplates, MailTemplates}
 import securesocial.core.RuntimeEnvironment
 import securesocial.core.authenticator.{CookieAuthenticatorBuilder, HttpHeaderAuthenticatorBuilder}
 import securesocial.core.providers.utils.PasswordHasher
 import securesocial.core.services.{AuthenticatorService, UserService}
 
-object SecurityEnvironment extends RuntimeEnvironment.Default[User] {
-  private lazy val mongoUserService = new MongoUserService
+object SecurityEnvironment
+  extends RuntimeEnvironment.Default[User]
+  with MongoSecureSocialUserServiceComponent
+  with MongoTokenRepositoryComponent {
+
+  lazy val mongoUserService = new MongoUserService
+  lazy val tokenRepository = mongoUserService
 
   override lazy val userService: UserService[User] = mongoUserService
   override lazy val authenticatorService: AuthenticatorService[User] = new AuthenticatorService[User](
