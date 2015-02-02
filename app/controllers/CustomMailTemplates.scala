bbc480745127cf87abd1c39157270e962f9c72f9
Tim Gordon <timothygordon32@gmail.com>
2015-02-02 20:50:49 +0000

    Add preview for security email templates, self-host logo.

--- app/controllers/CustomMailTemplates.scala
+++ app/controllers/CustomMailTemplates.scala
@@ -1,14 +1,57 @@
 package controllers
 
+import java.util.UUID
+
+import play.api.Play
+import play.api.Play.current
 import play.api.i18n.Lang
-import play.api.mvc.RequestHeader
-import play.twirl.api.Html
+import play.api.mvc.{Result, Action, RequestHeader}
+import play.twirl.api.{HtmlFormat, Html}
 import securesocial.controllers.MailTemplates
-import securesocial.core.{BasicProfile, IdentityProvider}
+import securesocial.core.{AuthenticationMethod, BasicProfile, IdentityProvider}
 import views.html.email._
 import views.txt.email._
 
-object CustomMailTemplates extends MailTemplates {
+object PreviewMailTemplates extends CustomMailTemplates {
+
+  val NamedUser = BasicProfile("", "", Some("John"), None, None, None, None, AuthenticationMethod.UserPassword)
+  val FakeToken = UUID.randomUUID().toString
+
+  def signUpEmail = Action { implicit request =>
+    preview(getSignUpEmail(FakeToken)._2.get _)
+  }
+
+  def alreadyRegisteredEmail = Action { implicit request =>
+    preview(getAlreadyRegisteredEmail(NamedUser)._2.get _)
+  }
+
+  def welcomeEmail = Action { implicit request =>
+    preview(getWelcomeEmail(NamedUser)._2.get _)
+  }
+
+  def unknownEmailNotice = Action { implicit request =>
+    preview(getUnknownEmailNotice()._2.get _)
+  }
+
+  def sendPasswordResetEmail = Action { implicit request =>
+    preview(getSendPasswordResetEmail(NamedUser, FakeToken)._2.get _)
+  }
+
+  def passwordChangedNoticeEmail = Action { implicit request =>
+    preview(getPasswordChangedNoticeEmail(NamedUser)._2.get _)
+  }
+
+  private def preview(x: () => HtmlFormat.Appendable): Result = {
+    if (Play.isDev)
+      Ok(x())
+    else NotFound
+  }
+}
+
+object CustomMailTemplates extends CustomMailTemplates
+
+trait CustomMailTemplates extends MailTemplates {
+
   def salutationText(salutation: String)(user: BasicProfile) =
     user.firstName.fold(s"$salutation,")(name => s"$salutation $name,")
 
@@ -22,11 +65,18 @@ object CustomMailTemplates extends MailTemplates {
   val welcomeHtml = salutationHtml("Welcome") _
 
   def baseUrl(implicit request: RequestHeader) = s"${routes.Home.index().absoluteURL(IdentityProvider.sslEnabled)}#"
+  def logoUrl(implicit request: RequestHeader) = routes.Assets.versioned("images/email.gif").absoluteURL(IdentityProvider.sslEnabled)
 
   def getSignUpEmail(token: String)(implicit request: RequestHeader, lang: Lang) = {
     val signUpTokenLink = s"$baseUrl/signup/$token"
     (Some(signUpEmailText(signUpTokenLink)),
-      Some(signUpEmailHtml(signUpTokenLink)))
+      Some(signUpEmailHtml(signUpTokenLink, logoUrl)))
+  }
+
+  def getAlreadyRegisteredEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = {
+    val resetTokenLink = s"$baseUrl/reset"
+    (Some(alreadyRegisteredEmailText(helloText(user), resetTokenLink)),
+      Some(alreadyRegisteredEmailHtml(helloHtml(user), resetTokenLink)))
   }
 
   def getWelcomeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = {
@@ -50,11 +100,5 @@ object CustomMailTemplates extends MailTemplates {
   def getPasswordChangedNoticeEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = {
     (Some(passwordChangedEmailText(helloText(user))),
       Some(passwordChangedEmailHtml(helloHtml(user))))
-  }
-
-  def getAlreadyRegisteredEmail(user: BasicProfile)(implicit request: RequestHeader, lang: Lang) = {
-    val resetTokenLink = s"$baseUrl/reset"
-    (Some(alreadyRegisteredEmailText(helloText(user), resetTokenLink)),
-      Some(alreadyRegisteredEmailHtml(helloHtml(user), resetTokenLink)))
   }
 }
